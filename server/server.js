6c10569bf55350a60dcbddd5202155b6da4bf3ab
Manikandan <manikandan@JokerPanda>
2024-10-28 16:07:59 +0530

    add * get in server.js

--- server/server.js
+++ server/server.js
@@ -215,9 +215,9 @@ app.post('/add-expense', verifyToken,async (req, res) => {
 });
 
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(path.resolve(), '../Client/dist', 'index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(path.resolve(), '../Client/dist', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,'0.0.0.0', () => {
